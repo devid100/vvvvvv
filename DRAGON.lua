21597f3a53662d8fc5b46cb123778420bf903ace
devid100 <82916614+devid100@users.noreply.github.com>
2021-06-01 15:46:04 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -2719,7 +2719,7 @@ local Text = [[
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text =  ' 𖣘 ⁽ＤＯＮＧＯＬ₎ 𖣘'   ,url="t.me/UU_DON"}},
+{{text = '  𖣘 ⁽ＤＯＮＧＯＬ₎ 𖣘   ',url="t.me/UU_DON"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token.. /sendPhoto?chat_id=  .. msg.chat_id_ ..  &photo=https://t.me/UU_DON&caption=  .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
