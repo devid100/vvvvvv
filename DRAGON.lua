554f69ef267ddf5ba39fdbd61bddd51aad74cae9
devid100 <82916614+devid100@users.noreply.github.com>
2021-06-01 17:37:21 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -11838,7 +11838,7 @@ if result.photos_[0] then
 sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_," اسمي "..Name_Bot.." ", msg.id_, msg.id_, "md")  
 end
 tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = bot_id, offset_ = 0, limit_ = 1 }, getpro, nil)  
-end
+end 
 if text == "صورتي"  then
 local my_ph = database:get(bot_id.."my_photo:status"..msg.chat_id_)
 if not my_ph then
