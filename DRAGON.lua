120471a3f86e72d636266537514478c73d06bbad
devid100 <82916614+devid100@users.noreply.github.com>
2021-06-19 23:17:35 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -13932,6 +13932,7 @@ local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
 end
+
 if text and text:match("^انطق (.*)$") then  
 local textntk = text:match("^انطق (.*)$")  
 UrlAntk = https.request('https://apiabs.ml/Antk.php?abs='..URL.escape(textntk)..'')  
