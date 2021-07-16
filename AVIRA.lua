44750e2818e5992ce1c97e78cd30816c54697678
devid100 <82916614+devid100@users.noreply.github.com>
2021-07-16 07:25:17 +0200

    Add files via upload

--- AVIRA.lua
+++ AVIRA.lua
@@ -11101,14 +11101,14 @@ end
 
 if text == 'بخ' then
 local Text = [[
-كسم ديفيد
+يوه خدتني اجدع
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '𓆩| ıl`ＡＶＩＲＡ´lı |𓆪', url="t.me/A_V_I_R_A_1"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendanimation?chat_id=' .. msg.chat_id_ .. '&animation=https://t.me/A_V_I_R_A_1/6&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..token..'/sendanimation?chat_id=' .. msg.chat_id_ .. '&animation=https://t.me/D_V1_D/96&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 
 if text and text:match("^وضع لقب (.*)$") and msg.reply_to_message_id_ ~= 0 and Constructor(msg) then
